2bc7cccb6fb56ddd393cab11bd26107b86af35d9
Majid Bhuiyan <majid.bhuiyan2011@gmail.com>
2025-12-03 16:14:16 +0600

    success screen done

---
+++ lib/presentation/auth/create_new_password/view/change_password_success.dart
@@ -0,0 +1,54 @@
+import 'package:anninawyss_o72194b15c03/core/constansts/app_icons.dart';
+import 'package:anninawyss_o72194b15c03/presentation/widgets/custom_button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../core/constansts/app_colors.dart';
+class ChangePasswordSuccess extends StatefulWidget {
+  const ChangePasswordSuccess({super.key});
+
+  @override
+  State<ChangePasswordSuccess> createState() => _ChangePasswordSuccessState();
+}
+
+class _ChangePasswordSuccessState extends State<ChangePasswordSuccess> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.whiteBackgroundColor,
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(AppIcons.passwordChangeSuccess, height: 150.h, width: 150.w,),
+              SizedBox(
+                height: 20.h,
+              ),
+              Text(
+                'Password Changed!',
+                style: TextStyle(
+                  fontSize: 24.sp,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 10.h,
+              ),
+              Text(
+                'Your password has been changed successfully.',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16.sp, color: Colors.grey) ,
+              ),
+              SizedBox(
+                height: 20.h,
+              ),
+              CustomButton(buttonText: "Go To Home", onTap: (){})
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
