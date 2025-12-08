d3f0892046512181dc712a47b4c10a12c95c8a12
Majid Bhuiyan <majid.bhuiyan2011@gmail.com>
2025-12-08 12:13:51 +0600

    update module two screen

---
+++ lib/presentation/all_module/module_two/view/modul_two_screen.dart
@@ -0,0 +1,74 @@
+import 'package:anninawyss_o72194b15c03/core/route/route_manager.dart';
+import 'package:anninawyss_o72194b15c03/presentation/widgets/custom_app_bar.dart';
+import 'package:anninawyss_o72194b15c03/presentation/widgets/custom_button.dart';
+import 'package:anninawyss_o72194b15c03/presentation/widgets/custom_content_button.dart';
+import 'package:anninawyss_o72194b15c03/presentation/widgets/custom_subtitle_text.dart';
+import 'package:anninawyss_o72194b15c03/presentation/widgets/custom_title_text.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../core/constansts/app_colors.dart';
+
+class ModuleTwoScreen extends StatelessWidget {
+  const ModuleTwoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.whiteBackgroundColor,
+      appBar: CustomAppBar(
+
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.r),
+        child: Container(
+          padding: EdgeInsets.all(16.r),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12.r),
+            color: AppColors.cardBackgroundColor,
+
+          ),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TitleText(title: "Understanding"),
+                  SizedBox(height: 8.h,),
+                  SubTitle(subtitle: "Welcome to your journey of healing. Here, you'll find the tools and support to rebuild your life."),
+                  SizedBox(height: 18.h,),
+                  CustomContentButton(text: "Self-test: Are you in a narcissistic relationship?", onTap: (){
+                    Navigator.pushNamed(context, Routes.selfTestScreenRoute);
+                  }),
+                  SizedBox(height: 18.h,),
+                  CustomContentButton(text: "What is narcissism?", onTap: (){
+                    Navigator.pushNamed(context, Routes.whatNarcissismRoute);
+                  }),
+                  SizedBox(height: 18.h,),
+                  CustomContentButton(text: "Narcissistic personality disorder", onTap: (){}),
+                  SizedBox(height: 18.h,),
+                  CustomContentButton(text: "Types of narcissism", onTap: (){}),
+                  SizedBox(height: 18.h,),
+                  CustomContentButton(text: "Important terms", onTap: (){}),
+                  SizedBox(height: 18.h,),
+                  CustomContentButton(text: "Cycle of narcissistic relationships", onTap: (){}),
+                  SizedBox(height: 18.h,),
+                  CustomContentButton(text: "Forms of psychological abuse", onTap: (){}),
+                  SizedBox(height: 18.h,),
+                  CustomContentButton(text: "Psychological & physical signs of narcissistic abuse", onTap: (){}),
+                  SizedBox(height: 18.h,),
+                  CustomContentButton(text: "What narcissistic abuse does to the nervous system", onTap: (){}),
+                  SizedBox(height: 18.h,),
+                  CustomContentButton(text: "Retraumatization through the environment", onTap: (){}),
+                  SizedBox(height: 24.h,),
+                  
+                  CustomButton(buttonText: "Where do you currently stand?", onTap: (){}),
+                  SizedBox(height: 16.h,),
+                  CustomButton(buttonText: "SOS Area", onTap: (){}),
+                ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
