9582dcef0ee1a876c0bdcc4d75e7f880c7528982
Majid Bhuiyan <majid.bhuiyan2011@gmail.com>
2025-12-10 15:53:14 +0600

    activate support environment

---
+++ lib/presentation/all_module/module_three/view/activate_support_environment.dart
@@ -0,0 +1,78 @@
+import 'package:anninawyss_o72194b15c03/presentation/widgets/custom_app_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../core/constansts/app_colors.dart';
+import '../../../../core/constansts/app_text_style.dart';
+import '../../../widgets/custom_button.dart';
+import '../../../widgets/custom_description_widget.dart';
+
+class ActivateSupportEnvironment extends StatefulWidget {
+  const ActivateSupportEnvironment({super.key});
+
+  @override
+  State<ActivateSupportEnvironment> createState() => _ActivateSupportEnvironmentState();
+}
+
+class _ActivateSupportEnvironmentState extends State<ActivateSupportEnvironment> {
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController _textEditingController = TextEditingController();
+    return Scaffold(
+      backgroundColor: AppColors.whiteBackgroundColor,
+      appBar: CustomAppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            SizedBox(height: 8.h,),
+            Container(
+              padding: EdgeInsets.all(16.r),
+              decoration: BoxDecoration(
+                color: AppColors.cardBackgroundColor,
+                borderRadius: BorderRadius.circular(12.r),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Text("Advantages of ending the relationship", style: AppTextStyles.nunitoS16BoldC2F2A29,),
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: ". ",
+                          style: TextStyle(
+                            fontSize: 30,   // Bigger dot
+                            color: Color(0Xff6B7280),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        TextSpan(
+                          text: "Welcome to your journey of healing. Here, you'll find the tools and support to rebuild your life.",
+                          style:AppTextStyles.interS16RegularC6B7280,
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 12.h,),
+                  CustomDescriptionTextField(hintText: "A healthy, respectful, supportive, and balanced partnership.", controller: _textEditingController),
+                  SizedBox(height: 16.h,),
+
+                ],
+
+              ),
+            ),
+            SizedBox(height: 18.h,),
+            Align(
+              alignment: Alignment.centerRight,
+              child: SizedBox(
+                  width: 140.w,
+                  child: CustomButton(buttonText: "Save", onTap: (){})),
+            ),
+            SizedBox(height: 18.h,),
+          ],
+        ),
+      ),
+    );
+  }
+}
